d67998d3a5a5cd4b79836ba0829a2bf0db063e85
Marc Lambrichs <mlambrichs@melange-it.nl>
2019-06-04 05:29:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# vagrant-R-neo4j
---
+++ puppet/manifests/default.pp
@@ -0,0 +1,10 @@
+lookup('classes', Array[String], 'first', ['roles::node']).include
+
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
+Package {
+  allow_virtual => true,
+}
+
+node default {
+}
